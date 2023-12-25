70010e01be936f9a098e603d8ad09c5d743fb6c9
Arief Ichwan Fanani <145969404+Arief-IF@users.noreply.github.com>
2023-12-25 21:37:01 +0700

    Add files via upload

---
+++ praktikum12/otentikasi.js
@@ -0,0 +1,15 @@
+const username = "arief"
+const password = "241204"
+
+function auth (){
+    let userInput = document.getElementById('username').value
+    let passwordInput = document.getElementById('password').value
+    let form = document.getElementById('form')
+
+    if(userInput == username && passwordInput == password){
+        alert("Login Beerhasil!")
+        form.submit()
+    } else {
+        alert("Login Gagal!")
+    }
+}
